3dc748d9aef184fab90d0388a832bef5bf0345f5
Nae Bara <natanaelbarag@gmail.com>
2022-02-18 10:13:43 +0200

    on test

--- src/main/java/com/app/AppApplication.java
+++ src/main/java/com/app/AppApplication.java
@@ -14,6 +14,11 @@ public class AppApplication {
 		return "Aplicatie pe azure bajetii mei! Timi Titus aici";
 	}
 
+
+	@GetMapping("/test")
+	public String test(){
+		return "test";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
